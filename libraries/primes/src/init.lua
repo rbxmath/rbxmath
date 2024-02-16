32f4acd6aa9ef844f61133fc435925b7508a078d
Ethan <ethanhcurtiss@gmail.com>
2024-02-16 00:48:56 -0500

    Fix Primes

--- libraries/primes/src/init.lua
+++ libraries/primes/src/init.lua
@@ -7,58 +7,52 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 local PrimeList = require(script.Parent.PrimeList)
 
 local Primes = {
-    primes = PrimeList
+	List = PrimeList,
 }
 
-function Primes:buildPrimesList (n : number) : { [number] : number }
-    local primesList = self.primes or {}
-    local max = primesList[#primesList]
-    local start
-    if max then
-        start = max + 1
-    else
-        start = 2
-    end
-    local primesList = {}
-    for i = start, n do
-        local isPrime = true
-        local sqrt = math.sqrt(i)
-        for k, v in ipairs(primesList) do
-            if k > sqrt then
-                break
-            end
-            isPrime = (i % v ~= 0)
-            if not isPrime then
-                break
-            end
-        end
-        if isPrime then
-            primesList[#primesList + 1] = i
-        end
-    end
-    return primesList
+function Primes.BuildPrimeList(n: number): { number }
+	for i = PrimeList[#PrimeList] + 1, n do
+		local isPrime = true
+		local sqrt = math.sqrt(i)
+
+		for k, v in PrimeList do
+			if k > sqrt then
+				break
+			end
+
+			isPrime = (i % v ~= 0)
+			if not isPrime then
+				break
+			end
+		end
+
+		if isPrime then
+			table.insert(PrimeList, i)
+		end
+	end
+
+	return PrimeList
 end
 
-function Primes:decompose (n : number) : { [number] : number }
-    local sqrt = math.sqrt(n)
-    local primeList = {}
-    if sqrt > self.primes[#self.primes] then
-        primeList = self:buildPrimesList(n)
-    else
-        primeList = self.primes
-    end
-    local decomp = {}
-    for k, v in ipairs(primeList) do
-        if v > n then
-            break
-        end
-        local temp = n
-        while temp % v == 0 do
-            temp = temp / v
-            decomp[#decomp + 1] = v
-        end
-    end
-    return decomp
+function Primes.Decompose(n: number): { number }
+	if math.sqrt(n) > PrimeList[#PrimeList] then
+		Primes.BuildPrimeList(n)
+	end
+
+	local decomp = {}
+	for k, v in PrimeList do
+		if v > n then
+			break
+		end
+
+		local temp = n
+		while temp % v == 0 do
+			temp /= v
+			table.insert(decomp, v)
+		end
+	end
+
+	return decomp
 end
 
 return Primes
